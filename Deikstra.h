94f64e0a32a3a75dcc3e3d5859f5c239a0a87718
Александр <ziminas1990@gmail.com>
2019-08-12 22:14:42 +0300

    Добавил комментарии в реализацию алгоритма

--- Deikstra.h
+++ Deikstra.h
@@ -8,6 +8,8 @@
 
 #include "IGraph.h"
 
+// Контекст узла хранит длину nShortestPath маршрута, который выходит из
+// некоторой начальной точки и приходит в данную точку от её соседа nPreviousNode
 struct NodeContext {
   NodeContext() = default;
   NodeContext(NodeId nodeId, uint32_t nShortestPathLength,
@@ -23,8 +25,11 @@ struct NodeContext {
   NodeId   nPreviousNode  = IGraph::InvalidNodeId();
 };
 
-// looks in list an element, that has minimal nShortestPath value, mark it as visited
-// and returns it
+// Вспомогательная функция
+// Данная функция просматривает контекст узлов, пути до которых известны, но
+// которые ещё не были посещены, находит узел с наиболее коротким путём до
+// него, возвращает его и удаляет из контекста
+static
 NodeContext visitNodeWithShortestPath(std::map<NodeId, NodeContext>& context)
 {
   if (context.empty())
@@ -52,50 +57,61 @@ NodeContext visitNodeWithShortestPath(std::map<NodeId, NodeContext>& context)
 static bool deikstra(IWeightedGraph const& graph, NodeId nStart, NodeId nFinish,
                      NodesVector& path)
 {
+  // В контексте context записаны узлы, расстояние до которых уже известно, но
+  // которые ещё НЕ были обработаны (посещены)
   std::map<NodeId, NodeContext> context;
-  // index - NodeId; if value is True, that means that node has been visited already
+  // Вектор, в котором отмечаются посещённые узлы. Индекс - номер узла,
+  // значение - факт посещения. Позволяет за O(1) определить, был ли узел
+  // посещён ранее
   std::vector<NodeContext> visitedNodes(graph.getTotalNodes());
 
+  // изначально в контексте только стартовый узел. С него и начнётся обработка
   context[nStart] = NodeContext(nStart, 0, IGraph::InvalidNodeId());
 
   while (!context.empty()) {
-    // looking for node, that has shortest path:
+    // Находим непосещённый узел с минимальной длиной пути до него, и берём
+    // его в обработку (помечаем как посещённый)
     NodeContext currentNode    = visitNodeWithShortestPath(context);
     NodeId      nCurrentNodeId = currentNode.nNodeId;
     visitedNodes[nCurrentNodeId] = currentNode;
     if (nCurrentNodeId == nFinish)
-      break;
+      break; // Выбранный узел является конечной точкой - маршрут найден.
 
     NodesVector neighbors;
     graph.getNeighbors(nCurrentNodeId, neighbors);
     if (neighbors.empty())
       continue;
 
+    // Проходим по всем соседям выбранного узла
     for (NodeId neighbor : neighbors) {
       if (visitedNodes[neighbor].isValid())
-        continue; // ignoring nodes, that has been handled already
+        continue; // Узлы, которые ранее были обработаны, игнорируются
 
+      // вычисляем найденный путь до соседа (путь до текущего узла + длина ребра)
       NodeContext& neighborContext = context[neighbor];
       uint32_t nEdgeLength     = graph.getEdge(nCurrentNodeId, neighbor);
       uint32_t nPathToNeighbor = currentNode.nShortestPath + nEdgeLength;
 
       if (!neighborContext.isValid() ||
           nPathToNeighbor < neighborContext.nShortestPath) {
-        // node is visited at first OR
-        // path to neighbor from current node is shorter, path, that was discovered before
+        // если узел ранее не был достижим, либо если новый найденный путь до него
+        // оказался короче найденного ранее, то запишем в контекст длину найденного пути
         neighborContext = NodeContext(neighbor, nPathToNeighbor, nCurrentNodeId);
       }
     }
   }
 
-  path.reserve(10); // ¯\_(ツ)_/¯
+  path.reserve(10); // Эвристическая оптимизация ¯\_(ツ)_/¯
 
+  // "Раскручиваем" маршрут, двигаемся от конечной точки к начальной,
+  // записывая порядок следования узлов
   NodeId nCurrentNodeId = nFinish;
   while(nCurrentNodeId != IGraph::InvalidNodeId()) {
     NodeContext const& nodeContext = visitedNodes[nCurrentNodeId];
     path.push_back(nodeContext.nNodeId);
     nCurrentNodeId = nodeContext.nPreviousNode;
   }
+  // Развернём порядок следования узлов
   std::reverse(path.begin(), path.end());
   return true;
 }
